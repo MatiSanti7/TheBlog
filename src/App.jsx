6fe11f7388939f3eb72c3e40c37fb1e61d5cc4a4
Matias <matiassantillan052@gmail.com>
2024-05-15 18:09:41 -0300

    install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
-import Blog from "./components/Blog/Blog";
+import Blog from "./components/Blog";
+import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import "./index.css";
 
@@ -11,15 +13,21 @@ const App = () => {
   };
 
   return (
-    <div
-      className={`${
-        darkMode ? "dark-mode" : "light-mode"
-      } 2xl:px-44 md:px-20 px-10 pt-28`}
-    >
-      <Nav toggleDarkMode={toggleDarkMode} />
-      <Blog></Blog>
-      <Footer></Footer>
-    </div>
+    <Router>
+      <div
+        className={`${
+          darkMode ? "dark-mode" : "light-mode"
+        } 2xl:px-44 md:px-20 px-10 pt-28`}
+      >
+        <Nav toggleDarkMode={toggleDarkMode} />
+        <Routes>
+          <Route path="/" element={<Blog />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+        <Footer></Footer>
+      </div>
+    </Router>
   );
 };
 
