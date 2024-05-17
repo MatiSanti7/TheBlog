d126239923bd7a00076f73488503421f60bbc8cf
Matias <matiassantillan052@gmail.com>
2024-05-17 14:12:02 -0300

    Fix Active Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -46,12 +46,16 @@ const Nav = ({ toggleDarkMode }) => {
   return (
     <header>
       <nav className="fixed top-0 left-0 flex items-center justify-between w-full px-10 pt-7 pb-7 2xl:px-44 md:px-20 nav-princ">
-        <strong className="text-2xl md:text-xl 2xl:text-4xl">
+        <a href="/" className="text-2xl font-bold md:text-xl 2xl:text-4xl">
           Matias Santillán
-        </strong>
+        </a>
         <div>
           <ul className="hidden gap-3 text-xl lg:gap-10 md:flex">
-            <li className={`px-2 pb-1 nav-item ${isActive("/blog")}`}>
+            <li
+              className={`px-2 pb-1 nav-item ${
+                isActive("/blog") || isActive("/")
+              }`}
+            >
               <a href="/blog" onClick={() => handleNavLinkClick("/blog")}>
                 Blog
               </a>
@@ -97,11 +101,13 @@ const Nav = ({ toggleDarkMode }) => {
         } left-0 flex flex-col items-center justify-end w-full h-screen bg-white nav`}
       >
         <div className="flex flex-col items-center justify-center gap-20 h-5/6">
-          <strong className="mb-20 text-3xl md:text-2xl ">
+          <a href="/" className="mb-20 text-3xl font-bold md:text-2xl ">
             Matias Santillán
-          </strong>
+          </a>
           <ul className="flex flex-col gap-10 text-2xl text-center">
-            <li className={`h-9 nav-item ${isActive("/blog")}`}>
+            <li
+              className={`h-9 nav-item ${isActive("/blog") || isActive("/")}`}
+            >
               <a href="/blog" onClick={() => handleNavLinkClick("/blog")}>
                 Blog
               </a>
